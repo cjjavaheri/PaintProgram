211f0de36e3785ad1cb014607d22667d0633f693
Javaheri, Cameron <cameron.javaheri@mines.sdsmt.edu>
2016-09-16 23:24:05 -0500

    Made functions for the color palette.

--- callbacks.cpp
+++ callbacks.cpp
@@ -5,15 +5,10 @@
  *
  * @brief A callback function for refreshing the display
  ******************************************************************************/
-void display()
+
+
+void Color_Palette()
 {
-    // Clear the window
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glutInitWindowSize(1024, 999);
-    glutInitWindowPosition(100, 100);
-
-    // Color palette
     DrawFilledRectangle(0, 0, 50, 50, Grey);
     DrawFilledRectangle(0, 50, 50, 100, Red);
     DrawFilledRectangle(0, 100, 50, 150, Green);
@@ -22,55 +17,11 @@ void display()
     DrawFilledRectangle(0, 250, 50, 300, Cyan);
     DrawFilledRectangle(0, 300, 50, 350, Yellow);
     DrawFilledRectangle(0, 350, 50, 400, White);
-	
-    // DrawLine( 10, 20, glutGet(GLUT_WINDOW_WIDTH) - 10,
-    //         glutGet(GLUT_WINDOW_HEIGHT) - 20, Yellow );
-    // DrawRectangle( 500, 400, 700, 500, Cyan );
-    // DrawFilledRectangle( 200, 100, 300, 300, Red );
-   //  DrawEllipse( 100, 50, 600, 200, Green );
-    // DrawFilledEllipse( 100, 50, 250, 450, Magenta );
 
-    // label display with text
-    DrawTextString( "OpenGL Demo", 32, 800 - 32, White );
-
-    // Make sure all the draw functions a complete to the buffer
-    glutSwapBuffers();
 }
 
-/***************************************************************************//**
- * @brief A callback function for handling keyboard input
- *
- * @param[in] key - the key that was pressed
- * @param[in] x   - the x-coordinate when the key was pressed
- * @param[in] y   - the y-coordinate when the key was pressed
- ******************************************************************************/
-void keyboard(unsigned char key, int x, int y)
+void Choose_Color(int button, int state, int x, int y)
 {
-    // Pressing the ESCAPE Key will exit from the main glut loop
-
-    int ScreenHeight = glutGet(GLUT_WINDOW_HEIGHT);
-
-    y = ScreenHeight - y;
-
-    if (key == ESCAPE_KEY)
-        glutLeaveMainLoop();
-    else
-        cout << "Key " << (char)key << " press detected at ["
-             << x << ", " << y << "]\n";
-}
-
-/***************************************************************************//**
- * @brief A callback function for handling mouse click events
- *
- * @param[in] button - the mouse button pushed
- * @param[in] state  - the state of the button (i.e. GLUT_UP, GLUT_DOWN)
- * @param[in] x      - the x-coordinate when the button is pressed
- * @param[in] y      - the y-coordinate when the button is pressed
- ******************************************************************************/
-void mouseClick(int button, int state, int x, int y)
-{
-    y = glutGet(GLUT_WINDOW_HEIGHT) - y;
-    // Mouse functionality for color palette.
     if (x >= 0 && x <= 50 && y >= 0 && y < 50)
     {
         DrawFilledRectangle(200, 200, 300, 300, Grey);
@@ -111,6 +62,70 @@ void mouseClick(int button, int state, int x, int y)
         DrawFilledRectangle(200, 200, 300, 300, White);
         glutSwapBuffers();
     }
+
+}
+
+
+void display()
+{
+    // Clear the window
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glutInitWindowSize(1024, 999);
+    glutInitWindowPosition(100, 100);
+
+    Color_Palette();
+	
+    // DrawLine( 10, 20, glutGet(GLUT_WINDOW_WIDTH) - 10,
+    //         glutGet(GLUT_WINDOW_HEIGHT) - 20, Yellow );
+    // DrawRectangle( 500, 400, 700, 500, Cyan );
+    // DrawFilledRectangle( 200, 100, 300, 300, Red );
+   //  DrawEllipse( 100, 50, 600, 200, Green );
+    // DrawFilledEllipse( 100, 50, 250, 450, Magenta );
+
+    // label display with text
+    DrawTextString( "OpenGL Demo", 32, 800 - 32, White );
+
+    // Make sure all the draw functions a complete to the buffer
+    glutSwapBuffers();
+}
+
+
+/***************************************************************************//**
+ * @brief A callback function for handling keyboard input
+ *
+ * @param[in] key - the key that was pressed
+ * @param[in] x   - the x-coordinate when the key was pressed
+ * @param[in] y   - the y-coordinate when the key was pressed
+ ******************************************************************************/
+void keyboard(unsigned char key, int x, int y)
+{
+    // Pressing the ESCAPE Key will exit from the main glut loop
+
+    int ScreenHeight = glutGet(GLUT_WINDOW_HEIGHT);
+
+    y = ScreenHeight - y;
+
+    if (key == ESCAPE_KEY)
+        glutLeaveMainLoop();
+    else
+        cout << "Key " << (char)key << " press detected at ["
+             << x << ", " << y << "]\n";
+}
+
+/***************************************************************************//**
+ * @brief A callback function for handling mouse click events
+ *
+ * @param[in] button - the mouse button pushed
+ * @param[in] state  - the state of the button (i.e. GLUT_UP, GLUT_DOWN)
+ * @param[in] x      - the x-coordinate when the button is pressed
+ * @param[in] y      - the y-coordinate when the button is pressed
+ ******************************************************************************/
+void mouseClick(int button, int state, int x, int y)
+{
+    y = glutGet(GLUT_WINDOW_HEIGHT) - y;
+    // Mouse functionality for color palette.
+   Choose_Color(button, state, x, y);
     cout << "MouseClick: Button = " << ButtonName[button] << " : State = "
          << ButtonState[state] << " : Location [" << x << ", " << y << "]\n";
 }
