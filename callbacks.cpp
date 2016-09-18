f966faa9d89e3c4b8ee984e80589874e80503e3b
Javaheri, Cameron <cameron.javaheri@mines.sdsmt.edu>
2016-09-17 20:03:01 -0500

    Fixed bug with drawing the rectangles.

--- callbacks.cpp
+++ callbacks.cpp
@@ -139,7 +139,7 @@ void mouseClick(int button, int state, int x, int y)
     {
         x_coordinate2 = x;
         y_coordinate2 = y;
-        static Rectangle rect((x_coordinate1 + x_coordinate2) / 2, (y_coordinate1 + 	y_coordinate2) / 2, MAGENTA , x_coordinate2 - x_coordinate1, y_coordinate2 - y_coordinate1);
+        Rectangle rect((x_coordinate1 + x_coordinate2) / 2, (y_coordinate1 + 	y_coordinate2) / 2, MAGENTA , x_coordinate2 - x_coordinate1, y_coordinate2 - y_coordinate1);
         Shape * some_shape = &rect;
         some_shape->draw();
 
